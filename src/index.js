d8eac3ab3bfe603dbe1495882f1803bdead4d944
Jose <Jrivera6869@yahoo.com>
2020-03-26 23:44:50 -0400

    skeleton complete

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import reducers from './reducers';
+
+const store = createStore(reducers, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <React.StrictMode>
